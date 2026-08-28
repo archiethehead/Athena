a2182044b9d3d0cc12e2e99e9422fe22efe1f4fc
Archie Healy <archiethehead@gmail.com>
2026-08-28 19:19:52 +0100

    Dynamic Linking
    Feat: Correctly set up dyanimc linking for the sandbox project, relying
    on the native engine code provided by Athena.Engine.

---
+++ Sandbox/Source/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <..\..\Engine\Include\Engine.h>
+
+int main() {
+
+	CAthenaEngine newEngine = CAthenaEngine();
+	
+	while (true) {
+	
+		newEngine.Update(1.0f);
+	
+	}
+
+	return 0;
+
+}
