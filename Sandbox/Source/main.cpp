d053559b7640ee025d9df249b2cab7c1d80d578d
Archie Healy <archiethehead@gmail.com>
2026-08-28 20:04:21 +0100

    Delta
    Feat: Implement a delta time system that gets Unix epoch time in
    milliseconds, subtracts that from the last frames epoch time, and
    expresses that as a fraction of 1000 milliseconds (1 second)

--- Sandbox/Source/main.cpp
+++ Sandbox/Source/main.cpp
@@ -1,14 +1,35 @@
-#include <cstdio>
+#include <chrono>
 #include <..\..\Engine\Include\Engine.h>
+
+constexpr float g_fMillisecondsInSeconds = 1000.0f;
+
+float g_fDelta = 0.0f;
+long long g_llUnixEpochMilliseconds = 0;
 
 int main() {
 
-	CAthenaEngine newEngine = CAthenaEngine();
+	std::chrono::milliseconds llMilliseconds = duration_cast<std::chrono::milliseconds>(
+		std::chrono::system_clock::now().time_since_epoch()
+	);
+	g_llUnixEpochMilliseconds = llMilliseconds.count();
 	
+	CAthenaEngine NewEngine = CAthenaEngine();
+
 	while (true) {
 	
-		newEngine.Update(1.0f);
-	
+		std::chrono::milliseconds llMilliseconds = duration_cast<std::chrono::milliseconds>(
+			std::chrono::system_clock::now().time_since_epoch()
+		);
+
+		long long llTimePassed = llMilliseconds.count();
+
+		llTimePassed = llTimePassed - g_llUnixEpochMilliseconds;
+		float fSecondsPassed = llTimePassed / g_fMillisecondsInSeconds;
+
+		NewEngine.Update(fSecondsPassed);
+
+		g_llUnixEpochMilliseconds = llMilliseconds.count();
+
 	}
 
 	return 0;
